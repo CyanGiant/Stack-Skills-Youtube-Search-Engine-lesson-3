2ae0a6b5d72dddc08f651b2d5b158ad83453adc3
James Bennett <J.bennett@gmx.com>
2015-08-05 16:24:15 -0700

    adds api get and serch functionality without population

--- js/script.js
+++ js/script.js
@@ -24,4 +24,43 @@ $(function(){
       },400, function(){});
     }
  });
+
+  $('#search-form').submit(function(e){
+    e.preventDefault();
+  });
 });
+
+
+function search(){
+  // clear results
+  $('#results').html('');
+  $('#buttons').html('');
+  //get form inputs
+  q = $('#query').val();
+
+  //run get request on api
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search",{
+      part: 'snippet, id',
+      q: q,
+      type: 'video',
+      key: 'AIzaSyDtiYiv_JGnMFkrzVj5YodXYgEICg3KLZo'},
+      function(data){
+        var nextPageToken = data.nextPageToken;
+        var prevPageToken = data.prevPageToken;
+
+          //log data
+        console.log(data);
+
+          //get output
+        $.each(data.items, function(i, item){
+          var output = getOutput(item);
+
+          // Display Results
+          $('#results').append(output);
+
+        });
+      }
+
+    );
+}
